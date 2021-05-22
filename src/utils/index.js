1a0793f7dba05ec845f8f0275be4cd9624aee60a
Axmin <axmin@bottle.com.np>
2021-05-22 15:44:35 +0545

    add permission util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,36 @@
-export const askLocationPermission = () =>
+export const askLocationPermission = (
+  options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  }
+) =>
   new Promise((resolve, reject) => {
-    resolve({ success: "lat lon" });
+    navigator.permissions
+      .query({ name: "geolocation" })
+      .then(function (result) {
+        switch (result.state) {
+          case "granted":
+            //NOTE: If granted then you can directly call your function here
+            return navigator.geolocation.getCurrentPosition(resolve);
+          case "denied":
+            //NOTE: If denied then you have to show instructions to enable location
+            return reject(result);
+          case "prompt":
+            return navigator.geolocation.getCurrentPosition(
+              resolve,
+              reject,
+              options
+            );
+          default:
+            const msg =
+              "Something unexpected happened when getting location permission from user";
+            alert(mssg);
+            console.log(msg, result);
+        }
+
+        result.onchange = function () {
+          console.log(result.state, "state on change");
+        };
+      });
   });
