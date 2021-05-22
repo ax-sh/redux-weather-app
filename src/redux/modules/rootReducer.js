8a02d037545f09bbc4a84d4ff67fcb899764fd98
Axmin <axmin@bottle.com.np>
2021-05-22 16:48:11 +0545

    init redux

---
+++ src/redux/modules/rootReducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+
+const weather = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  weather,
+});
