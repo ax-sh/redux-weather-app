b36cad82a7e799b2607d1e388b1a8a34b5de0f2c
Axmin <axmin@bottle.com.np>
2021-05-22 17:26:49 +0545

    add redux done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,47 @@ import React from "react";
 import "./App.scss";
 import { askLocationPermission } from "../utils";
 import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  fetchWeatherData,
+  setUserLocationPermission,
+} from "../redux/modules/weather/actions";
 
 function App() {
-  const [location, setLocation] = React.useState({ lat: null, lon: null });
-
-  React.useEffect(() => {
+  const weather = useSelector((state) => state.weather);
+  const dispatch = useDispatch();
+  const askPermission = () => {
+    dispatch(fetchWeatherData({ lat: 5, lon: 4 }));
     askLocationPermission()
       .then(({ coords }) => {
         const { latitude: lat, longitude: lon } = coords;
         console.log(coords, lat, lon);
         toast.success(`Location access ${coords}`);
+        dispatch(setUserLocationPermission("granted"));
       })
       .catch((e) => {
         console.error(e);
         toast.error(`ERROR Location access ${e.state}`);
+        dispatch(setUserLocationPermission("denied"));
       });
-  }, []);
+  };
+
+  React.useEffect(askPermission, []);
+
+  const hasPermission = weather.userLocationPermission === "granted";
 
   return (
     <div className="App">
       <header className="App-header">
-        <pre>{JSON.stringify(location, null, 4)}</pre>
+        {!hasPermission && (
+          <h1>
+            Enable location permission to fetch the weather data in your
+            location
+          </h1>
+        )}
+        <pre>{JSON.stringify(weather, null, 4)}</pre>
+        {/* <WeatherApp data={weather} /> */}
       </header>
     </div>
   );
