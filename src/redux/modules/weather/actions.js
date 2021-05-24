c56a1bce83ba872a5426faae20b12b66c63704f4
Axmin <axmin@bottle.com.np>
2021-05-24 21:09:07 +0545

    add actions

--- src/redux/modules/weather/actions.js
+++ src/redux/modules/weather/actions.js
@@ -1,6 +1,16 @@
+export const WEATHER_DATA_REQUEST = "WEATHER_DATA_REQUEST";
+export const WEATHER_DATA_SUCCESS = "WEATHER_DATA_SUCCESS";
+export const WEATHER_DATA_FAILURE = "WEATHER_DATA_FAILURE";
+
 export const fetchWeatherData = ({ lat, lon }) => {
   return { type: "FETCH_WEATHER_DATA", payload: { lat, lon } };
 };
 export const setUserLocationPermission = (payload) => {
   return { type: "SET_USER_LOCATION_PERMISSION", payload };
+};
+
+export default {
+  WEATHER_DATA_FAILURE,
+  WEATHER_DATA_SUCCESS,
+  WEATHER_DATA_REQUEST,
 };
