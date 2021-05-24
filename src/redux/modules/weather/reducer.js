f4cc7116fea10444658abc125c49de030619e18e
Axmin <axmin@bottle.com.np>
2021-05-24 21:09:18 +0545

    add reducer

--- src/redux/modules/weather/reducer.js
+++ src/redux/modules/weather/reducer.js
@@ -1,16 +1,22 @@
+import actions from "./actions";
+
 const initialState = {
   userLocationPermission: null,
   status: null,
   location: { lat: null, lon: null },
+  loading: true,
 };
 
 const weather = (state = initialState, action) => {
   switch (action.type) {
-    case "FETCH_WEATHER_DATA":
-      return { ...state, params: action.payload };
+    case actions.WEATHER_DATA_REQUEST:
+      return { ...state, loading: true };
+    case actions.WEATHER_DATA_SUCCESS:
+      return { ...state, loading: false, weather: action.payload };
+    case actions.WEATHER_DATA_FAILURE:
+      return { ...state, loading: false, error: "Loading Failed" };
     case "SET_USER_LOCATION_PERMISSION":
       return { ...state, userLocationPermission: action.payload };
-
     default:
       return state;
   }
